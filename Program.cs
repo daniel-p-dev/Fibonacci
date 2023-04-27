c1d636e227ac931b2248e81908fd992371c8ea4d
Daniel Pacheco <daniel.pacheco.dev@gmail.com>
2023-04-27 14:17:44 -0300

    codigo simples usando o loop while

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿int primeiroNumero = 0, segundoNumero = 1, proximoNumero = 0;
+bool detectado = false;
+
+Console.WriteLine("Digite um número inteiro: ");
+int n = int.Parse(Console.ReadLine());
+
+while (proximoNumero <= n)
+{
+    if(proximoNumero == n)
+    {
+        Console.WriteLine($"o número {n} pertence à sequência de Fibonacci");
+        detectado = true;
+        break;
+    }
+    else
+    {
+        proximoNumero = primeiroNumero + segundoNumero;
+        primeiroNumero = segundoNumero;
+        segundoNumero = proximoNumero;
+    }
+
+}
+
+if (!(detectado))
+{
+    Console.WriteLine($"O número {n} não pertence a sequência de Fibonacci");
+}
